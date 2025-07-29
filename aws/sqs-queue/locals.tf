4a601262c5b58734e5db6a0e9203c53e1d1793dd
Thiago Panini <panini.development@gmail.com>
2025-07-28 22:24:03 -0300

    docs: restoring docs for lambda-layer module

--- aws/sqs-queue/locals.tf
+++
@@ -1,8 +0,0 @@
-/* -----------------------------------------------------------------------------
-  FILE: locals.tf
-  MODULE: aws/sqs-queue
-
-  DESCRIPTION:
-    Local values for the SQS queue module, including default tags and computed
-    values for resource configuration.
------------------------------------------------------------------------------ */
