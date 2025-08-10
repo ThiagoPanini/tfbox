64b24519e3ef8703c212377480675968e77954f8
Thiago Panini <panini.development@gmail.com>
2025-08-09 21:48:45 -0300

    feat(aws/lambda-function): updating call for aws/lambda-layer module for v0.4.0

--- aws/lambda-function/lambda_layers.tf
+++ aws/lambda-function/lambda_layers.tf
@@ -15,6 +15,6 @@
 # Calling the Lambda Layers module to create layers if enabled
 module "aws_lambda_layers" {
   count      = var.create_lambda_layers ? 1 : 0
-  source     = "git::https://github.com/ThiagoPanini/tfbox.git?ref=aws/lambda-layer/v0.3.0"
+  source     = "git::https://github.com/ThiagoPanini/tfbox.git?ref=aws/lambda-layer/v0.4.0"
   layers_map = var.layers_map
 }
