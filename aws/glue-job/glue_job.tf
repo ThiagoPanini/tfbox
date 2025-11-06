93a1a8aea6c9e4af174318c8b69a2cdeac4599cb
Thiago Panini <panini.development@gmail.com>
2025-11-05 23:12:12 -0300

    feat: initializing the glue job module by uploading glue assets to s3

---
+++ aws/glue-job/glue_job.tf
@@ -0,0 +1,13 @@
+/* -----------------------------------------------------------------------------
+  FILE: glue_job.tf
+  MODULE: aws/glue-job
+
+  DESCRIPTION:
+    This Terraform module provisions a AWS Glue Job with flexible configuration
+    and best practices.
+
+  RESOURCES:
+    - aws_glue_job.this:
+        Provisions the Glue Job.
+----------------------------------------------------------------------------- */
+
